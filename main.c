6eda2cf6b18aaca150c4dc6b036269f196fb0dfd
Jack Pedicone <jackpedicone@gmail.com>
2018-02-09 22:43:42 -0500

    Major changes to ADC10, in progress of finishing main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 //*****************************************//
-// Heat Enclosure with the MSP430G2553 1.0 //
-// Steven Portley and Jack Pedicone		   //
-// Last updated 2/6/2018				   //			
+// Heat Enclosure with the MSP430G2553 	   //
+// Jack Pedicone		   				   //
+// Last updated 2/9/2018				   //			
 //*****************************************//
 
 // SUBJECT TO CHANGE
@@ -12,49 +12,55 @@
 void TimerAInit(void);
 void TimerBInit(void);
 void ADC10Init(void);
+void fanInit(void);
 void fanControl(void);
 void tempInit(void);
 void tempControl(void);
+void tempSwitch(void);
 
 //Variable declaration//
 unsigned volatile int		adc_in 		= 0;
 volatile float				tempC 		= 0;
-volatile float				tempC_set 	= 0;
-volatile float				tempF 		= 0;
 volatile float				voltage 	= 0;
+
+volatile float				tempO		= 0;
+volatile float				tempI		= 0;
 
 void tempInit()
 {
-	//temperature probe needs to be configured for the ADC pin of the G2
-	P1OUT &= ~BIT0;        // Clear LED to start
-	P1DIR |= BIT0;         // P1.0 output
-	P1SEL1 |= BIT1;        // Configure P1.1 for ADC
-	P1SEL0 |= BIT1;
+	//sensor 1
+	P2DIR |= BIT0; //set pin 2.0 and 2.1 as outputs
+	P2DIR |= BIT1;
+	
+	P2OUT |= BIT0;
 }
 
-void tempControl()
+void tempControl(void)
 {
 	
 }
 
-void fanControl(void)
+void tempSwitch(void)
 {
-	//For pin 1.4
-	P1DIR |= BIT4;		//Pin 1.4
-	P1SEL1 &= ~BIT4;	//control which functions will be connected or multiplexed onto the pins.  
-	P1SEL0 |= BIT4;		//The higher four bits have as their function to enable JTAG or to disable it.
+	P2OUT ^= BIT0;
+	P2OUT ^= BIT1;
+}
+
+void fanInit(void)
+{
+	P1DIR |= BIT1;		//Pin 1.1
+	P1SEL1 &= ~BIT1;	//control which functions will be connected or multiplexed onto the pins.  
+	P1SEL0 |= BIT1;		//The higher four bits have as their function to enable JTAG or to disable it.
 }
 
 void ADC10Init(void)
 {
-	ADC10CTL0 = ADC10SHT0_2 + ADC10ON;      // Set sample time for the ADC10 control register 0, turn ADC10 on
-	ADC10CTL1 = ADC10SHP;                   // Enable sample timer for the ADC10 control register 1
-	ADC10CTL2 |= ADC10RES_2;                // 10-bit conversion results
-	ADC10MCTL0 = ADC10INCH_5 | ADC10VRSEL_1;// Vref+ = , Input
-	ADC10IER0 |= ADC10IE0;                  // Enable ADC conv complete interrupt
+	ADC10CTL0 = ADC10IE + ADC10SHT0_2 + ADC10ON + MSC;      // Set sample time for the ADC10 control register 0, turn ADC10 on
+	ADC10CTL1 = ADC10SHP + ADC10INCH_3 + ADC10SSEL_0 + CONSEQ_2;                   // Enable sample timer for the ADC10 control register 1
+	ADC10AE0 |= BIT3; 
 }
 
-void TimerAInit(void)  //Timer used the outside probe
+void TimerAInit(void)  //Timer used the temperature sensor
 {
 	TA0CCTL0 = CCIE;					//Disable timer Interrupt
 	TA0CCTL1 = OUTMOD_3;				//Set/Reset when the timer counts to the TA0CCR1 value, reset for TA0CCR0
@@ -63,13 +69,22 @@ void TimerAInit(void)  //Timer used the outside probe
 	TA0CTL = TASSEL_1 + MC_1 + ID_3;	//Enable Timer A with SMCLK
 }
 
+void TimerBInit(void)  //Timer used for the fan
+{
+	TB0CCTL1 = OUTMOD_3;			//Set OUTMOD_3 (set/reset) for CCR1
+									//Set initial values for CCR1 (255 -> 254)
+	TB0CCR1 = 0xFF;				    //reset and set immediately (May change to slower clock)
+	TB0CCR0 = 255 - 1;			    //Set CCR0 for a ~1kHz clock.
+	TB0CTL = TBSSEL_2 + MC_1;		//Enable Timer B0 with SMCLK and up mode. 1MHz
+}
+
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;		// Stop WDT
 	fanControl();					// Fan Pin Initialization
-	tempInit();				// Temperature GPIO Initialization
-	TimerAInit();					// Timer A Function call UART
-	TimerBInit();					// Timer B Function call PWM
+	tempInit();						// Temperature GPIO Initialization
+	TimerAInit();					
+	TimerBInit();					
 
 	while (REFCTL0 & REFGENBUSY);            // If ref generator busy, WAIT
 	REFCTL0 |= REFVSEL_0 + REFON;           // Enable internal 1.2 reference
@@ -81,6 +96,10 @@ int main(void)
 
 	while (1)
 	{
+		if (!(tempO == 0) && !(tempI == 0))
+		{
+			tempControl();
+		}
 		__bis_SR_register(LPM0 + GIE); // Enter LPM0, interrupts enabled
 		__no_operation(); // For debugger
 	}
@@ -96,15 +115,29 @@ __interrupt void TIMER0_A0_ISR(void)
 
 
 //ADC ISR
-#pragma vector=ADC12_B_VECTOR
+#pragma vector=ADC10_B_VECTOR
 __interrupt void ADC10ISR(void)
 {
-	adc_in = ADC10MEM0;		    //set ADC12MEM to variable
+	adc_in = ADC10MEM0;		    //set ADC10MEM to variable
+	
 	voltage = adc_in * 0.00029;	    //converts ADC to voltage
 	tempC = voltage / 0.01;		     //converts voltage to Temp C
-	tempF = ((9 * tempC) / 5) + 32;     //Temp C to Temp F
-	while (!(UCA0IFG&UCTXIFG));
-	UCA0TXBUF = tempC;		     //change to =tempF to output in Fahrenheit 
+	
+	switch (P2OUT)
+	case 
+	if (P2OUT == BIT0)
+	{
+		tempO = tempC;
+		P2OUT &= ~BIT0;
+		P2OUT |= BIT1;
+	}
+	else
+	if (P2OUT == BIT1)
+	{
+		tempI = tempC;
+		P2OUT &= ~BIT1;
+		P2OUT |= BIT0;
+	}
 }
 
 
